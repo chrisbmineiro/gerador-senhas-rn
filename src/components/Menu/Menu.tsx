10c57e6a9ed3362be1934e449232577d8dc2a24d
chrisbmineiro <chrisbmineiro@gmail.com>
2024-09-04 21:11:17 -0300

    base do projeto

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { styles } from './MenuStyles'
+import { View, Text } from 'react-native'
+
+export default function Menu() {
+  return (
+    <View style={styles.container}>
+        <Text>Ola</Text>
+    </View>
+  )
+}
